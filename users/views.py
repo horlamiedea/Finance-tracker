86feb724794400b8ade6422cf12b29e36ee34c82
horlamiedea <horlamiedea@gmail.com>
2025-06-03 13:04:43 +0100

    added users endpoint

--- users/views.py
+++ users/views.py
@@ -1,6 +1,17 @@
 from rest_framework import generics, permissions
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class UserDetailView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
