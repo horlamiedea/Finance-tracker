aad3a95cd0a71a528e47c4a71c8ade27179b2fa3
horlamiedea <horlamiedea@gmail.com>
2025-09-19 19:14:14 +0100

    fixes

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -8,17 +8,9 @@ class TransactionCategorySerializer(serializers.ModelSerializer):
         model = TransactionCategory
         fields = ['id', 'name']
 class ReceiptSerializer(serializers.ModelSerializer):
-    uploaded_image_url = serializers.SerializerMethodField()
-
     class Meta:
         model = Receipt
         fields = ['id', 'uploaded_image_url', 'extracted_text', 'items', 'upload_date']
-
-    def get_uploaded_image_url(self, obj):
-        request = self.context.get('request')
-        if obj.uploaded_image and request:
-            return request.build_absolute_uri(obj.uploaded_image.url)
-        return None
 
 class TransactionSerializer(serializers.ModelSerializer):
     receipt = ReceiptSerializer(read_only=True)
